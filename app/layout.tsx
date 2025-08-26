ec42d27fd66bb4c8cc7fce5202cc8111d082b0cf
CotiSolutions <cotisolutions@gmail.com>
2025-08-26 12:31:52 -0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,14 @@ export default function RootLayout({
                         gtag('config', 'G-C2LNFYWTB4');
                     `}
                 </Script>
+
+                {/* Favicons and App Icons */}
+                <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="MCMA" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
             </head>
             <body className="antialiased overflow-x-hidden">
                 <Navbar />
