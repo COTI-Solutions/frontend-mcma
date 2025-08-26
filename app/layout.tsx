f9c4f3c81292317318031de0e14b9afc62c64324
CotiSolutions <cotisolutions@gmail.com>
2025-08-26 12:24:57 -0300

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
@@ -15,6 +16,21 @@ export default function RootLayout({
 }) {
     return (
         <html lang="es">
+            <head>
+                {/* Google tag (gtag.js) */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-C2LNFYWTB4"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-C2LNFYWTB4');
+                    `}
+                </Script>
+            </head>
             <body className="antialiased overflow-x-hidden">
                 <Navbar />
                 {children}
